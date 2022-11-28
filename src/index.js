bb5e50656a79abcce5a73e3f301651dc5b1b0a25
windows <windows@microsoft.com>
2022-11-28 12:29:40 +0530

    add route

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Myhome from './components/Home';
 import Mybout from './components/About';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Myhome />
-  <Mybout></Mybout>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+        <Route path="home" element={<Myhome />} />
+        <Route path='about' element={<Mybout/>} />
+
+    </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 

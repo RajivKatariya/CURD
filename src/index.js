d9b15dad250fd01c6d8a26287dacc8d3ff9043f7
windows <windows@microsoft.com>
2022-12-01 12:26:24 +0530

    folder structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Navbar from './Navbar';
-import About from './About';
-import Error from './Error';
+import Travallandingpage from './modules/traval/travallandingpage';
+import Educationlandingpage from './modules/education/Educationlandingpage';
+import Welcome from './Welcome';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar />
-
-        <Routes>
-          <Route path='/home' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='*' element={<Error/>} />
-
-        </Routes>
+      <Routes>
+        <Route path='' element={<Welcome/>}/>
+        <Route path='traval' element={<Travallandingpage/>}/>
+        <Route path='education' element={<Educationlandingpage />}/>
+        
+      </Routes>
+    
+    
     </BrowserRouter>
+
   </React.StrictMode>
 );
 

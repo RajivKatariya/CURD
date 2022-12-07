520a1d6eb25ebab01516c2ec9b98a6d1f6bbfc54
Jaydeep Rana <jaydeepran49@gmail.com>
2022-12-07 11:43:02 +0530

    New Cjanges

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Travallandingpage from './modules/traval/travallandingpage';
 import Educationlandingpage from './modules/education/Educationlandingpage';
 import Welcome from './Welcome';
-
+import Mynav from './Mynav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Mynav />
       <Routes>
+        
         <Route path='' element={<Welcome/>}/>
         <Route path='traval' element={<Travallandingpage/>}/>
         <Route path='education' element={<Educationlandingpage />}/>
         
       </Routes>
     
-    
+      
     </BrowserRouter>
 
   </React.StrictMode>

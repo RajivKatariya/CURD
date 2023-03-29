8452b80b428df0689e2a5112d253e6618936882a
windows <windows@microsoft.com>
2023-03-29 19:53:26 +0530

    new prokect

--- backend/db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DB = "mongodb+srv://mernuser:supperpassword@cluster0.cqczybv.mongodb.net/mydatabase";
-
-mongoose.connect(DB,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>console.log("connection start")).catch((error)=> console.log(error.message));
